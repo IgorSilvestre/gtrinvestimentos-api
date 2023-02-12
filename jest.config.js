7fb292a363414c1019c420a103cfc971eb13744e
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-02-12 00:43:07 -0300

    chore: implemented jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['./src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(./src/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
