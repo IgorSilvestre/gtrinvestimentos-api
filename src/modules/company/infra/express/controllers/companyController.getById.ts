1ecb739c80f6a0b64bc8cfce2b9a7d5834a03aa6
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-02-12 23:04:07 -0300

    refactor: getById company types

--- src/modules/company/infra/express/controllers/companyController.getById.ts
+++ src/modules/company/infra/express/controllers/companyController.getById.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express'
 import { CompanyService } from '../../../service/companyService'
 import { AppError } from '../../../../../shared/AppError'
+import { ZCompany } from '../../../interfaces-validation/ZCompany'
 
 export async function getById (req: Request, res: Response) {
   const { id } = req.params
-  const company = await CompanyService.getById(id)
+  const company: ZCompany | AppError = await CompanyService.getById(id)
   if (company instanceof AppError) {
     return res.status(company.status).json({ error: company.message })
   }
 
-  return res.status(200).json(company)
+  return res.status(200).json(company as ZCompany)
 }
--- src/modules/company/service/services/companyService.getById.ts
+++ src/modules/company/service/services/companyService.getById.ts
@@ -1,13 +1,16 @@
 import { CompanyRepository } from '../../infra/mongo/CompanyRepository'
 import { AppError } from '../../../../shared/AppError'
 import { errorMessageKeys } from '../../../../shared/keys/errorMessageKeys'
+import { ZCompany } from '../../interfaces-validation/ZCompany'
 
-export async function getById (id: string): Promise<any> {
+export async function getById (id: string): Promise<ZCompany | AppError> {
   try {
-    const company = await CompanyRepository.getById(id)
+    const company: ZCompany | null = await CompanyRepository.getById(id)
+
     if (company === null) return new AppError(
       { clientMessage: errorMessageKeys.company.notFound },
       404)
+    return company as ZCompany
   } catch (err) {
     return new AppError(
       { clientMessage: errorMessageKeys.company.notFound, apiError: err },
