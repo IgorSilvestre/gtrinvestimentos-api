ca52476f41313bb44af2d61866d838f6ce65361a
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-02-12 23:05:33 -0300

    refactor: remove company types

--- src/modules/company/infra/express/controllers/companyController.remove.ts
+++ src/modules/company/infra/express/controllers/companyController.remove.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express'
 import { CompanyService } from '../../../service/companyService'
 import { AppError } from '../../../../../shared/AppError'
+import { DeleteResult } from 'mongodb'
 
 export async function remove (req: Request, res: Response) {
   const { id } = req.params
-  const result = await CompanyService.remove(id)
+  const result: DeleteResult | AppError = await CompanyService.remove(id)
 
   if (result instanceof AppError) return res.status(result.status).json({ error: result.message });
-  console.log(result)
   return res.status(204).json(result)
 }
--- src/modules/company/service/services/companyService.remove.ts
+++ src/modules/company/service/services/companyService.remove.ts
@@ -1,12 +1,11 @@
 import { DeleteResult } from 'mongodb'
-import { IAppError } from '../../../../shared/interfaces/appError/IAppError'
 import { CompanyRepository } from '../../infra/mongo/CompanyRepository'
 import { AppError } from '../../../../shared/AppError'
 import { errorMessageKeys } from '../../../../shared/keys/errorMessageKeys'
 
-export async function remove (id: string): Promise<DeleteResult | IAppError> {
+export async function remove (id: string): Promise<DeleteResult | AppError> {
   try {
-    return await CompanyRepository.remove(id)
+    return await CompanyRepository.remove(id) as DeleteResult
   } catch (err) {
     return new AppError({ clientMessage: errorMessageKeys.company.notRemoved, apiError: err })
   }
