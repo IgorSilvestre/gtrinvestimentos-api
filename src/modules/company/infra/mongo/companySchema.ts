5826506253d3c00e315f1e0002527e2fe5d55675
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-05-04 16:06:41 -0300

    changed company schema

--- src/modules/company/infra/mongo/companySchema.ts
+++ src/modules/company/infra/mongo/companySchema.ts
@@ -1,51 +1,32 @@
 import mongo from '../../../../shared/database/database'
 
 const companySchema = new mongo.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  website: {
-    type: String,
-  },
-  documentType: {
-    type: String,
-  },
-  documentNumber: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  state: {
-    type: String,
-  },
-  city: {
-    type: String,
-  },
-  zipCode: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    required: true,
-  },
-  lastUpdated: {
-    type: Date,
-    required: true,
-    default: Date.now(),
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [String],
+        required: false,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    target: {
+        type: String,
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        required: true,
+    },
+    lastUpdated: {
+        type: Date,
+        required: true,
+        default: Date.now(),
+    },
 })
 
 export const companyModel = mongo.model('Company', companySchema)
--- src/modules/company/interfaces-validation/ZCompany.ts
+++ src/modules/company/interfaces-validation/ZCompany.ts
@@ -1,32 +1,24 @@
 import { z } from 'zod'
 
-const stringOptions = {
-  invalid_type_error: 'Type must be a string',
+function stringOptions(type: string) {
+    return {
+        invalid_type_error: `${type} must be a string`,
+    }
 }
 
 export const companyValidation = z.object({
-  _id: z.any().optional(),
-  name: z
-    .string({
-      required_error: 'Name is required',
-      invalid_type_error: 'Name must be a string',
-    })
-    .min(2),
-  type: z.string({
-    required_error: 'Type is required',
-    invalid_type_error: 'Type must be a string',
-  }),
-  website: z.string(stringOptions).url().optional(),
-  documentType: z.string(stringOptions).optional(),
-  documentNumber: z.string(stringOptions).optional(),
-  phone: z.string(stringOptions).optional(),
-  email: z.string(stringOptions).email().min(5).optional(),
-  address: z.string(stringOptions).optional(),
-  state: z.string(stringOptions).optional(),
-  city: z.string(stringOptions).optional(),
-  zipCode: z.string(stringOptions).optional(),
-  lastUpdated: z.date().optional(),
-  createdAt: z.date().optional(),
+    _id: z.any().optional(),
+    name: z
+        .string({
+            required_error: 'Name is required',
+            invalid_type_error: 'Name must be a string',
+        })
+        .min(2),
+    tags: z.array(z.string(stringOptions('tags'))).optional(),
+    description: z.string(stringOptions('description')).optional(),
+    target: z.string(stringOptions('target')).optional(),
+    lastUpdated: z.date().optional(),
+    createdAt: z.date().optional(),
 })
 
 export type ZCompany = z.infer<typeof companyValidation>
