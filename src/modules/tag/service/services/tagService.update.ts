6e52a23eb6a454d54f1f081faa4e921538031e04
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-05-08 09:24:37 -0300

    style: change param name in tagService.update

--- src/modules/tag/service/services/tagService.update.ts
+++ src/modules/tag/service/services/tagService.update.ts
@@ -3,13 +3,13 @@ import { TagRepository } from '../../infra/mongo/repository/TagRepository'
 import { AppError } from '../../../../shared/AppError'
 import { errorMessageKeys } from '../../../../shared/keys/errorMessageKeys'
 
-export async function update (id: string, tagUpdatedData: ZTag): Promise<ZTag | AppError> {
-  try {
-    const tag: ZTag | null = await TagRepository.update(id, tagUpdatedData)
+export async function update(id: string, tagUpdatedData: ZTag): Promise<ZTag | AppError> {
+    try {
+        const tag: ZTag | null = await TagRepository.update(id, tagUpdatedData)
 
-    if (tag === null) return new AppError({ clientMessage: 'returned: NULL' })
-    return tag as ZTag
-  } catch (err) {
-    return new AppError({ clientMessage: errorMessageKeys.tag.notUpdated, apiError: err })
-  }
+        if (tag === null) return new AppError({ clientMessage: 'returned: NULL' })
+        return tag as ZTag
+    } catch (err) {
+        return new AppError({ clientMessage: errorMessageKeys.tag.notUpdated, apiError: err })
+    }
 }
