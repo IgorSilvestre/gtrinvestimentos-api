49911a860c9fb05685ceb7aafc9f9e0731060495
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-05-16 19:42:07 -0300

    fix: change 'lastUpdated' attribute in tag update

--- src/modules/tag/infra/mongo/repository/repositories/tagRepository.update.ts
+++ src/modules/tag/infra/mongo/repository/repositories/tagRepository.update.ts
@@ -3,7 +3,7 @@ import { tagModel } from '../../tagSchema'
 
 export async function update (_id: string, data: ZTag): Promise<ZTag | null> {
   try {
-    return tagModel.findOneAndUpdate({ _id }, { $set: data }, { new: true })
+    return tagModel.findOneAndUpdate({ _id }, { $set: { ...data, lastUpdated: Date.now() } }, { new: true })
   }
   catch (err) {
     throw new Error(err as string)
