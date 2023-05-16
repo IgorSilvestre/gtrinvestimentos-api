541a05bcc546ec5b232d4df126cbd87bb73d2edc
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-05-16 19:57:15 -0300

    feat: function to parse ISODate to the Brazil standard

---
+++ src/shared/utils/parseISODateToBrazilSTD.ts
@@ -0,0 +1,10 @@
+const { parseISO, format } = require('date-fns');
+const { utcToZonedTime } = require('date-fns-tz');
+
+export function parseISODateToBrazilSTD (dataISO: string | undefined) {
+  if (!dataISO) return 'Registro sem data';
+  const data = parseISO(dataISO);
+  console.log('data', data)
+  const dataGMTMinus3 = utcToZonedTime(data, 'America/Sao_Paulo');
+  return format(dataGMTMinus3, 'dd/MM/yyyy HH:mm:ss');
+}
