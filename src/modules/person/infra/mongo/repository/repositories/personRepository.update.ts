a3e7b7ec4ee576b58f344483bb25551dde9dc7ac
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-05-16 19:42:02 -0300

    fix: change 'lastUpdated' attribute in person update

--- src/modules/person/infra/mongo/repository/repositories/personRepository.update.ts
+++ src/modules/person/infra/mongo/repository/repositories/personRepository.update.ts
@@ -3,7 +3,7 @@ import { personModel } from '../../personSchema'
 
 export async function update (_id: string, data: ZPerson): Promise<ZPerson | null> {
   try {
-    return personModel.findOneAndUpdate({ _id }, { $set: data }, { new: true })
+    return personModel.findOneAndUpdate({ _id }, { $set: { ...data, lastUpdated: Date.now() } }, { new: true })
   }
   catch (err) {
     throw new Error(err as string)
