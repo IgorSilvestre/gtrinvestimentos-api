c1bdf39c192f76b346eed6272e61241ce77b9d77
anselmo <orotel.aaos@gmail.com>
2023-06-22 20:11:07 -0300

    fix: parseSheetCompanies

--- Scripts anselmo/populate-database/empresas/parseSheetCompanies.js
+++ Scripts anselmo/populate-database/empresas/parseSheetCompanies.js
@@ -1,14 +1,47 @@
 
-import { CompanyService } from '../../../src/modules/company/service/companyService'
+import { CompanyService } from '/home/orotel/dev/gtrinvestimentos-api/src/modules/company/service/companyService'
+import { TagService } from '/home/orotel/dev/gtrinvestimentos-api/src/modules/tag/service/tagService'
+import {normalizedCompanies } from '/home/orotel/dev/gtrinvestimentos-api/Scripts anselmo/populate-database/empresas/MAIN.js'
 
-
-
-export async function parseSheetCompanies(request,response){
+async function createCompany (comp) {
+  // console.log('comp', comp)
   const obj = {
-    "name":"kinea",
+    name: comp.nome,
+    // "tags":company.area,
   }
-    const criar = await CompanyService.create(obj)
-    console.log('CRIAR', criar)
-    response.send(criar)
+  try {
+    if(typeof comp.nome !== 'string') {
+      console.log('BROKEN COMPANY >>>>')
+      return
+    }
+    return await CompanyService.create(obj)
+  } catch (err) {
+    console.log(err)
+    return Error(err)
+  }
+
+  // console.log('CRIAR', com
+}
+
+export async function parseSheetCompanies (request,response){
+  const tags = await TagService.getAll() // MONTA REGEX PRA BUSCAR O NOME DA TAG AQUI NESSE ARRAY
+
+  // console.log(tags)
+  const createdCompanies = await Promise.all(normalizedCompanies().map(async (company) => {
+    const createdCompanyOrError = await createCompany(company) 
+    if(createdCompanyOrError instanceof Error) return
+    return createdCompanyOrError
+    // // console.log(await createCompany(company))
+    // if (companyCreatedOrError instanceof Error) return 
+    // console.log(companyCreatedOrError)
+    // return companyCreatedOrError
+  }))
+
+  // console.log(createdCompanies)
+  response.status(201).json(createdCompanies)
+  
+
+//   response.status(501).json(gotError);
+  
 
 }
