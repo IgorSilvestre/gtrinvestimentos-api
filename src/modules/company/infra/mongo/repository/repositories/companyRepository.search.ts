f9cbff9b28c213540b08c003fd49a8afcdea6731
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-07-14 14:49:38 -0300

    feat: company multifilter

--- src/modules/company/infra/mongo/repository/repositories/companyRepository.search.ts
+++ src/modules/company/infra/mongo/repository/repositories/companyRepository.search.ts
@@ -1,16 +1,27 @@
-import { companyModel, ZCompanyModel } from '../../companySchema'
+import { regexForSearch } from '../../../../../../shared/functions/regexForSearch'
+import { companyModel } from '../../companySchema'
 
-export async function search (searchParams: unknown) {
+interface ISearchParams {
+  tags: string[]
+  query: string
+}
+export async function search(queryParams: ISearchParams) {
+  const { tags, query } = queryParams
+
   try {
-    const companies: ZCompanyModel[] = await companyModel
-      .find({tags: { $all: searchParams?.tags}})
+    const searchParams: any = {}
+
+    if (tags) searchParams.tags = { $all: tags }
+    if(query) searchParams.name = { $regex: regexForSearch(query) }
+
+    const companies = await companyModel
+      .find(searchParams)
       .sort({ createdAt: -1 })
       .populate('tags')
       .populate('employees')
-    
+
     return companies
-  }
-  catch (err) {
+  } catch (err) {
     throw new Error(err as string)
   }
 }
