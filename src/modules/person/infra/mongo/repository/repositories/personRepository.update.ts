05f44f5d9214753540b409d9c6c58f2cda89e8db
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-07-14 16:51:26 -0300

    feat: person update - populate tags & company

--- src/modules/person/infra/mongo/repository/repositories/personRepository.update.ts
+++ src/modules/person/infra/mongo/repository/repositories/personRepository.update.ts
@@ -3,7 +3,10 @@ import { personModel } from '../../personSchema'
 
 export async function update (_id: string, data: ZPerson): Promise<ZPerson | null> {
   try {
-    return personModel.findOneAndUpdate({ _id }, { $set: { ...data, lastUpdated: Date.now() } }, { new: true })
+    return personModel
+    .findOneAndUpdate({ _id }, { $set: { ...data, lastUpdated: Date.now() } }, { new: true })
+    .populate('tags')
+    .populate('company')
   }
   catch (err) {
     throw new Error(err as string)
