3495f92f0ffab92187977d1cddfd692b4b389c3f
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-08-02 18:24:48 -0300

    fix: definetly removed table-scripts

--- src/tableScripts/utils/arrIncludeRegex.ts
+++
@@ -1,6 +0,0 @@
-import { testRegexForSearch } from "../../shared/functions/testRegexForSearch";
-
-export function arrIncludeRegex (word: string, arr: string[]){
-  if(!arr) return false
-  return arr.some((testWord: string) => testRegexForSearch(word, testWord));
-}
