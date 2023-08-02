814302ebd4eeae823b892086fae526d7258d7138
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-08-02 18:28:16 -0300

    fix: removed table-scripts routes

--- src/http/routes/company.routes.ts
+++ src/http/routes/company.routes.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express'
 import { CompanyController } from '../../modules/company/infra/express/companyController'
-import { main } from '../../tableScripts/companies/main'
 
 export const companyRouter = Router()
 
 companyRouter.post('/', CompanyController.create)
 
-
-companyRouter.post('/send-table', main) // Table Scripts
 
 companyRouter.get('/all', CompanyController.getAll)
 
--- src/http/routes/tag.routes.ts
+++ src/http/routes/tag.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express'
 import { TagController } from '../../modules/tag/infra/express/tagController'
-import { createTags } from '../../tableScripts/tags/createTags'
-
 
 export const tagRouter = Router()
 
 tagRouter.post('/', TagController.create)
-
-// TABLE SCRIPTS
-tagRouter.get('/createTags', createTags)
 
 // GET REQUESTS
 tagRouter.get('/all', TagController.getAll)
