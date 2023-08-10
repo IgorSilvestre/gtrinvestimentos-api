6608aba2c1656ef8d789904b0d95aba44ae0f6ab
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-10 15:37:33 -0300

    feat: error messages for services registerEmployee, unregisterEmployee

--- src/shared/keys/errorMessageKeys.ts
+++ src/shared/keys/errorMessageKeys.ts
@@ -2,6 +2,9 @@ export const errorMessageKeys = {
   searchFailed: 'Não foi possível validar se registro ja existe',
   alreadyExists: 'Registro já existe',
   company: {
+    cantRegisterEmployee: 'Não foi possível vincular funcionário',
+    cantUnregisterEmployee: 'Não foi possível desvincular funcionário',
+    cantGetCompany: 'Não foi possível pegar registro de empresa',
     notCreated: 'Não foi possível criar empresa',
     notFound: 'Empresa não encontrada!',
     notUpdated: 'Não foi possível atualizar empresa',
@@ -14,6 +17,8 @@ export const errorMessageKeys = {
     notRemoved: 'Não foi possível remover tag'
   },
   person: {
+    cantSaveEmployee: 'Não foi possível salvar funcionário',
+    notGetCompanyForPerson: 'Não foi possível pegar empresa para cadastra-la em usuário',
     notCreated: 'Não foi possível criar pessoa',
     notFound: 'Pessoa não encontrada!',
     notUpdated: 'Não foi possível atualizar pessoa',
