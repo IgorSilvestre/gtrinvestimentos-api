7851cd1c1a3d3b636bca6e3193bae9ea3b9affc7
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-14 11:15:28 -0300

    feat: mergeTwoStringArrays util function

---
+++ src/shared/functions/mergeTwoStringArrays.ts
@@ -0,0 +1,16 @@
+export function mergeTwoStringArrays(
+  arr1: string[] | undefined,
+  arr2: string[] | undefined,
+): string[] | undefined {
+  if (arr1 && arr2) {
+    const mergedArrs = [...arr1, ...arr2]
+    const uniqueElements = mergedArrs.filter(
+      (element, index) => mergedArrs.indexOf(element) === index,
+    )
+    return uniqueElements
+  }
+
+  if (arr1) return arr1
+  if (arr2) return arr2
+  return undefined
+}
