a735891757fd9c2d7561a6a7231a7892fe70873f
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-14 18:51:44 -0300

    feat: sync company - person update

--- src/modules/person/service/services/personService.update.ts
+++ src/modules/person/service/services/personService.update.ts
@@ -2,14 +2,78 @@ import { ZPerson } from '../../interfaces-validation/ZPerson'
 import { PersonRepository } from '../../infra/mongo/repository/PersonRepository'
 import { AppError } from '../../../../shared/AppError'
 import { errorMessageKeys } from '../../../../shared/keys/errorMessageKeys'
+import { CompanyRepository } from '../../../company/infra/mongo/repository/CompanyRepository'
+import { CompanyService } from '../../../company/service/companyService'
+import { ZCompany } from '../../../company/interfaces-validation/ZCompany'
 
-export async function update (id: string, updatedPersonData: ZPerson): Promise<ZPerson | AppError> {
+interface IGetPerson extends Omit<ZPerson, 'company'> {
+  company: ZCompany
+}
+
+export async function update(
+  id: string,
+  updatedPersonData: ZPerson,
+): Promise<ZPerson | AppError> {
+  let person: IGetPerson
+  let error
+  // get Person
   try {
-    const person: ZPerson | null = await PersonRepository.update(id, updatedPersonData)
+    person = await PersonRepository.getById(id) as any
+  } catch (err) {
+    return new AppError({
+      clientMessage: errorMessageKeys.person.notFound,
+      apiError: err,
+    })
+  }
 
-    if (person === null) return new AppError({ clientMessage: 'returned: NULL' })
+  if (person?.company && person?.company?._id.toString() !== updatedPersonData.company) {
+    let isUnregistered
+
+    // if company changed, unregister employee from old company
+    try {
+      await CompanyRepository.updateWithPull(person?.company._id, {
+        employees: id,
+      })
+      isUnregistered = true
+    } catch (err) {
+      error = {
+        clientMessage: errorMessageKeys.company.cantUnregisterEmployee,
+        apiError: err,
+      }
+      console.log(error)
+    }
+
+    if (isUnregistered) {
+      // register employee to new company
+      try {
+        await CompanyService.registerEmployee(
+          id,
+          updatedPersonData.company as string,
+        )
+      } catch (err) {
+        return new AppError({
+          clientMessage: errorMessageKeys.company.cantRegisterEmployee,
+          apiError: err,
+        })
+      }
+    }
+  }
+
+  // update Person
+  try {
+    const person: ZPerson | null = await PersonRepository.update(
+      id,
+      updatedPersonData,
+    )
+    if (person === null)
+      return new AppError({ clientMessage: 'returned: NULL' })
+    
+    if(error) person.error = error.clientMessage
     return person as ZPerson
   } catch (err) {
-    return new AppError({ clientMessage: errorMessageKeys.person.notUpdated, apiError: err })
+    return new AppError({
+      clientMessage: errorMessageKeys.person.notUpdated,
+      apiError: err,
+    })
   }
 }
