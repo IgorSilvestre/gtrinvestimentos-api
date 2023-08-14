7be12ea40afa10bd74082de58329a265bf655dc0
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-14 18:43:44 -0300

    feat: add errorMessageKeys

--- src/shared/keys/errorMessageKeys.ts
+++ src/shared/keys/errorMessageKeys.ts
@@ -1,5 +1,5 @@
 export const errorMessageKeys = {
-  searchFailed: 'Não foi possível validar se registro ja existe',
+  checkIfExistsFailed: 'Não foi possível validar se registro ja existe',
   alreadyExists: 'Registro já existe',
   company: {
     cantRegisterEmployee: 'Não foi possível vincular funcionário',
@@ -8,20 +8,19 @@ export const errorMessageKeys = {
     notCreated: 'Não foi possível criar empresa',
     notFound: 'Empresa não encontrada!',
     notUpdated: 'Não foi possível atualizar empresa',
-    notRemoved: 'Não foi possível remover empresa'
+    notRemoved: 'Não foi possível remover empresa',
   },
   tag: {
     notCreated: 'Não foi possível criar tag',
     notFound: 'Tag não encontrada!',
     notUpdated: 'Não foi possível atualizar tag',
-    notRemoved: 'Não foi possível remover tag'
+    notRemoved: 'Não foi possível remover tag',
   },
   person: {
-    cantSaveEmployee: 'Não foi possível salvar funcionário',
-    notGetCompanyForPerson: 'Não foi possível pegar empresa para cadastra-la em usuário',
+    cantGetPerson: 'Não foi possível pegar registro de pessoa',
     notCreated: 'Não foi possível criar pessoa',
     notFound: 'Pessoa não encontrada!',
     notUpdated: 'Não foi possível atualizar pessoa',
-    notRemoved: 'Não foi possível remover pessoa'
-  }
+    notRemoved: 'Não foi possível remover pessoa',
+  },
 }
