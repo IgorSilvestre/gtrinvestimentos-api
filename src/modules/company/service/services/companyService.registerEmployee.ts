ca2b95b44cf0c50a11d89a251968a7659a2e268e
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-14 18:47:18 -0300

    feat: service - registerEmployee

--- src/modules/company/service/services/companyService.registerEmployee.ts
+++ src/modules/company/service/services/companyService.registerEmployee.ts
@@ -1,28 +1,29 @@
 import { AppError } from '../../../../shared/AppError'
-import { mergeTwoStringArrays } from '../../../../shared/functions/mergeTwoStringArrays'
+// import { mergeTwoStringArrays } from '../../../../shared/functions/mergeTwoStringArrays'
 import { errorMessageKeys } from '../../../../shared/keys/errorMessageKeys'
 import { ZPerson } from '../../../person/interfaces-validation/ZPerson'
 import { PersonService } from '../../../person/service/personService'
-import { ZCompany } from '../../interfaces-validation/ZCompany'
+// import { ZCompany } from '../../interfaces-validation/ZCompany'
 import { CompanyService } from '../companyService'
+import { converStringToObjectIdMongo } from '../../../../shared/functions/convertStringToObjectIdMongo'
 
 export async function registerEmployee(
   employeeId: string,
   companyId: string,
 ): Promise<true | AppError> {
-  let company: ZCompany | AppError
+  // let company: ZCompany | AppError
   let person: ZPerson | AppError
 
-  // Get company
-  try {
-    company = await CompanyService.getById(companyId)
-    if (company instanceof AppError) throw Error
-  } catch (err) {
-    return new AppError({
-      clientMessage: errorMessageKeys.company.cantGetCompany,
-      apiError: err,
-    })
-  }
+  // // Get company
+  // try {
+  //   company = await CompanyService.getById(companyId)
+  //   if (company instanceof AppError) throw Error
+  // } catch (err) {
+  //   return new AppError({
+  //     clientMessage: errorMessageKeys.company.cantGetCompany,
+  //     apiError: err,
+  //   })
+  // }
 
   // Get person
   try {
@@ -34,28 +35,24 @@ export async function registerEmployee(
       apiError: err,
     })
   }
-  // TODO REMOVE MERGE TWO ARRAYS - NEED TO GET COMPANY????
+  
   // Add employee tags to company
-  try {
-    await CompanyService.update(companyId, {
-      tags: mergeTwoStringArrays(company?.tags as string[], person?.tags),
-    } as ZCompany)
-  } catch (err) {
-    return new AppError({
-      clientMessage: errorMessageKeys.company.notUpdated + ' (tags)',
-      apiError: err,
-    })
+  if(person?.tags) {
+    try {
+      await CompanyService.addToSet(companyId, {
+        tags: { $each: converStringToObjectIdMongo(person?.tags)},
+      })
+    } catch (err) {
+      return new AppError({
+        clientMessage: errorMessageKeys.company.notUpdated + ' (tags)',
+        apiError: err,
+      })
+    }
   }
-
+  
   // Register employee
   try {
-    const updatedCompanyEmployees = {
-      employees:
-        company.employees && company.employees.length > 0
-          ? [...company.employees, employeeId]
-          : [employeeId],
-    }
-    await CompanyService.update(companyId, updatedCompanyEmployees as ZCompany)
+    await CompanyService.addToSet(companyId, { employees: converStringToObjectIdMongo(employeeId) })
   } catch (err) {
     return new AppError({
       clientMessage: errorMessageKeys.company.cantRegisterEmployee,
