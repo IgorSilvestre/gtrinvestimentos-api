c4eec7be79ae875117327a8f86a0f0b5178e4850
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-14 18:47:29 -0300

    feat: service - unregisterEmployee

--- src/modules/company/service/services/companyService.unregisterEmployee.ts
+++ src/modules/company/service/services/companyService.unregisterEmployee.ts
@@ -1,29 +1,18 @@
 import { AppError } from '../../../../shared/AppError'
 import { errorMessageKeys } from '../../../../shared/keys/errorMessageKeys'
-import { ZCompany } from '../../interfaces-validation/ZCompany'
-import { CompanyService } from '../companyService'
+import { CompanyRepository } from '../../infra/mongo/repository/CompanyRepository'
 
 export async function unregisterEmployee(
   employeeId: string,
   companyId: string,
 ): Promise<true | AppError> {
   try {
-    const company: ZCompany | AppError = await CompanyService.getById(companyId)
-    if (company instanceof AppError) {
-      throw Error(errorMessageKeys.company.cantGetCompany)
-    }
-    const updatedCompanyEmployees: any = {
-      employees:
-        company?.employees && company?.employees.length > 0
-          ? company?.employees.filter((employee) => employee !== employeeId)
-          : [],
-    }
-    await CompanyService.update(companyId, updatedCompanyEmployees)
+    await CompanyRepository.updateWithPull(companyId, { employees: employeeId })
+    return true
   } catch (err) {
     return new AppError({
       clientMessage: errorMessageKeys.company.cantUnregisterEmployee,
       apiError: err,
     })
   }
-  return true
 }
