2bb47b3cc91a20038d09a1c688f1a7103c8cec24
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-14 18:45:09 -0300

    feat: sort alphabetically

--- src/modules/company/infra/mongo/repository/repositories/companyRepository.getAll.ts
+++ src/modules/company/infra/mongo/repository/repositories/companyRepository.getAll.ts
@@ -1,11 +1,15 @@
 import { companyModel, ZCompanyModel } from '../../companySchema'
 
-export async function getAll () {
+export async function getAll() {
   try {
-    const companies: ZCompanyModel[] = await companyModel.find().sort({ createdAt: -1 }).populate('tags').populate('employees')
+    const companies: ZCompanyModel[] = await companyModel
+      .find()
+      .sort({ label: 1 })
+      .collation({ locale: 'en_US', strength: 2 })
+      .populate('tags')
+      .populate('employees')
     return companies
-  }
-  catch (err) {
+  } catch (err) {
     throw new Error(err as string)
   }
 }
