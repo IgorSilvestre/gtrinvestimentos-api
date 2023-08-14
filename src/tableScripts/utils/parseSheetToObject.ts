9b6505692c300713f1d7c323efd53055671900c0
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-14 11:16:59 -0300

    fix: sheet path

--- src/tableScripts/utils/parseSheetToObject.ts
+++ src/tableScripts/utils/parseSheetToObject.ts
@@ -1,7 +1,7 @@
 import XLSX from 'xlsx'
 
 export function parseSheetToArrayOfObjects (tabName: string): any[] {
-  const workbook = XLSX.readFile("/Users/igorsilvestre/Documents/dev/github-repos/gtrinvestimentos-api/src/tableScripts/utils/companySheet.xlsx");
+  const workbook = XLSX.readFile("/home/user/ff/gtrinvestimentos-api/src/tableScripts/utils/companySheet.xlsx");
   const worksheet = workbook.Sheets[tabName];
   return  XLSX.utils.sheet_to_json(worksheet)
 }
