748aa307331c82e11c08b69173448164c4cb1403
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-14 18:48:27 -0300

    refactor: prettier

--- src/modules/person/infra/mongo/repository/repositories/personRepository.getById.ts
+++ src/modules/person/infra/mongo/repository/repositories/personRepository.getById.ts
@@ -1,11 +1,13 @@
 import { ZPerson } from '../../../../interfaces-validation/ZPerson'
 import { personModel } from '../../personSchema'
 
-export async function getById (id: string): Promise<ZPerson> {
+export async function getById(id: string): Promise<ZPerson> {
   try {
-    return await personModel.findById(id).populate('tags').populate('company') as ZPerson
-  }
-  catch (err) {
+    return (await personModel
+      .findById(id)
+      .populate('tags')
+      .populate('company')) as ZPerson
+  } catch (err) {
     throw new Error(err as string)
   }
 }
