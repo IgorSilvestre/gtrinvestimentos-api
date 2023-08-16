d378c4d5dda0dc3947d1c2f592e71aea5512a790
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-16 17:42:07 -0300

    feat: people sheet - base parse

--- src/http/routes/person.routes.ts
+++ src/http/routes/person.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import { PersonController } from '../../modules/person/infra/express/personController'
+import { sendPeopleToDB } from '/home/user/ff/gtrinvestimentos-api/src/tableScripts/people/people'
 
 export const personRouter = Router()
 
 personRouter.post('/', PersonController.create)
+
+personRouter.get('/table', sendPeopleToDB)
 
 personRouter.get('/all', PersonController.getAll)
 
