1db693572383a5eee48bcb3b72cc9b6f01491b20
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-08-18 15:12:33 -0300

    refactor: correct interface ZPerson

--- src/tableScripts/people/parsePeople.ts
+++ src/tableScripts/people/parsePeople.ts
@@ -1,12 +1,16 @@
+import { ZPerson } from '../../modules/person/interfaces-validation/ZPerson'
 import { IPersonSheet } from './IPersonSheet'
 
 export function normalizedPeople(person: IPersonSheet) {
-  let obj: IPersonSheet = {}
+  let obj: ZPerson = {}
 
-  if (person.Nome) obj.Nome = person.Nome
-  if (person.empresa) obj.empresa = person.empresa
-  if (person.emails) obj.emails = person.emails
-  if (person.telefones) obj.telefones = person.telefones
+  if (person.Nome) obj.name = person.Nome
+  if (person.empresa) obj.company = person.empresa
+  if (person.emails && person.emails.length > 0) {
+    if (person.emails[1].length > 1) obj.email = person.emails[1]
+    if (person.emails[0].length > 1) obj.email = person.emails[0]
+  }
+  // if (person.telefones) obj.telefones = person.telefones
   if (person.tags) obj.tags = person.tags
   if (person.target) obj.target = person.target
 
