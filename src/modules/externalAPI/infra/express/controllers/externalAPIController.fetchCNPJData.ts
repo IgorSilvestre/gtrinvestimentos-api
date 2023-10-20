df3d28ffdbddd5d056928d559a392cd65c5dcacb
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-10-20 14:01:48 -0300

    feat: add fetch CNPJ data external API endpoint

--- src/modules/externalAPI/infra/express/controllers/externalAPIController.fetchCNPJData.ts
+++ src/modules/externalAPI/infra/express/controllers/externalAPIController.fetchCNPJData.ts
@@ -1,14 +1,24 @@
-import { Request, Response } from "express";
-import axios from "axios";
+import { Request, Response } from 'express'
+import { removeSpecialCharacters } from '../../../../../shared/functions/removeSpecialCharacters'
+import { externalAPIService } from '../../../service/externalAPIService'
+import { AppError } from '../../../../../shared/AppError'
 
 export async function fetchCNPJData(req: Request, res: Response) {
-  const { cnpj } = req.params;
+  const { cnpj } = req.params
+
+  if (!cnpj) return res.status(400).json({ error: 'CNPJ is required' })
+
+  const cnpjNormalized = removeSpecialCharacters(cnpj)
   try {
-    const response = await axios.get(`https://www.receitaws.com.br/v1/cnpj/${cnpj}`);
-    const data = response.data;
-    return res.status(200).json(data);
+    const CNPJData = await externalAPIService.fetchCNPJData(cnpjNormalized)
+
+    if (CNPJData instanceof AppError) {
+      return res.status(CNPJData.status).json({ error: CNPJData.message })
+    }
+
+    return res.status(200).json(CNPJData)
   } catch (error) {
-    console.error("Error fetching data:", error);
-    return res.status(500).json({ error: "Error fetching data" });
+    console.error('Error fetching data:', error)
+    return res.status(500).json({ error: 'Error fetching data' })
   }
 }
