d6fa2c19d8df5e2e7013a20a57bba7840374dfbe
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-11-10 11:09:59 -0300

    feat: add total pages and total companies to response (#12)

--- src/modules/company/infra/express/controllers/companyController.get.ts
+++ src/modules/company/infra/express/controllers/companyController.get.ts
@@ -2,12 +2,15 @@ import { Request, Response } from 'express'
 import { AppError } from '../../../../../shared/AppError'
 import { CompanyService } from '../../../service/companyService'
 import { normalizeCompanies } from '../../../interfaces-validation/normalizeCompanies'
-import { ICompanyDocument } from '../../../interfaces-validation/ICompanyModel'
+import { ICompaniesPaginated } from '../../../interfaces-validation/ICompaniesPaginated'
 
 export async function get (req: Request, res: Response) {
-  const companies: ICompanyDocument[] | AppError = await CompanyService.get(req.query)
+  const companies: ICompaniesPaginated | AppError = await CompanyService.get(req.query)
   if (companies instanceof AppError) {
     return res.status(companies.status).json({ error: companies.message })
   }
-  return res.status(200).json(normalizeCompanies(companies))
+  return res.status(200).json({
+    ...companies,
+    data: normalizeCompanies(companies.data),
+  })
 }
