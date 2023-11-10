1d04bffd0959864fbc67e3e5c940f6bae6489e5d
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-11-10 17:38:57 +0000

    feat: added validation, security and cache

--- src/modules/company/infra/mongo/repository/repositories/companyRepository.get.ts
+++ src/modules/company/infra/mongo/repository/repositories/companyRepository.get.ts
@@ -2,34 +2,76 @@ import { defaultValues } from '../../../../../../shared/defaultValues'
 import { IPaginationParams } from '../../../../../../shared/interfaces/IPaginationsParams'
 import { companyModel } from '../../companySchema'
 import { ICompaniesPaginated } from '../../../../interfaces-validation/ICompaniesPaginated'
+import { AppError } from '../../../../../../shared/AppError'
+import NodeCache from 'node-cache'
+import { ICompanyDocument } from '../../../../interfaces-validation/ICompanyModel'
+
+async function countTotalCompanies() {
+  try {
+    return await companyModel.countDocuments()
+  } catch (err) {
+    console.log(new AppError({ apiError: err, clientMessage: '' }))
+    return 0
+  }
+}
+
+const myCache = new NodeCache();
+
+async function fetchCompanies(page: number, limit: number) {
+ const key = `company-get-${page}-${limit}`;
+
+ // Try to get the data from cache
+ const cachedData = myCache.get(key);
+ if (cachedData) return cachedData as ICompanyDocument[];
+
+ try {
+   const data = await companyModel
+     .find()
+     .sort({ createdAt: -1 })
+     .collation({ locale: 'en_US', strength: 2 })
+     .populate('tags')
+     .populate('employees')
+     .skip((page - 1) * limit)
+     .limit(limit)
+     .lean();
+
+   myCache.set(key, data, 300);
+
+   return data;
+ } catch (err) {
+   throw new Error(err as string);
+ }
+}
 
 
 export async function get(
   paginationParams: IPaginationParams,
 ): Promise<ICompaniesPaginated> {
-  const page = paginationParams?.page || defaultValues.paginationPage
-  const limit = paginationParams?.limit || defaultValues.paginationLimit
+  const limit =
+    paginationParams?.limit && paginationParams?.limit > 0
+      ? paginationParams?.limit
+      : defaultValues.paginationLimit
 
-  const totalCompanies = await companyModel.countDocuments()
+  const totalCompanies = await countTotalCompanies()
   const totalPages = Math.ceil(totalCompanies / limit)
 
-  try {
-    const companies = await companyModel
-      .find()
-      .sort({ label: 1 })
-      .collation({ locale: 'en_US', strength: 2 })
-      .populate('tags')
-      .populate('employees')
-      .skip((page - 1) * limit)
-      .limit(limit)
-      .lean()
+  const page =
+    typeof paginationParams?.page == 'number' &&
+    paginationParams?.page &&
+    paginationParams?.page > 0 &&
+    paginationParams?.page <= totalPages
+      ? paginationParams?.page
+      : defaultValues.paginationPage
 
-    return {
-      data: companies,
-      totalCompanies,
-      totalPages,
-    }
-  } catch (err) {
-    throw new Error(err as string)
+  const previousPage = page > 1 ? page - 1 : null
+  const nextPage = page < totalPages ? page + 1 : null
+
+  const companies = await fetchCompanies(page, limit)
+  return {
+    data: companies,
+    totalCompanies,
+    totalPages,
+    previousPage,
+    nextPage,
   }
 }
