6451d7e18b70daf83dc821197d067180c73280f2
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2023-11-13 13:10:17 +0000

    fix: repository company getById lean()

--- src/modules/company/infra/mongo/repository/repositories/companyRepository.getById.ts
+++ src/modules/company/infra/mongo/repository/repositories/companyRepository.getById.ts
@@ -6,7 +6,8 @@ export async function getById(id: string): Promise<ICompanyDocument> {
     return (await companyModel
       .findById(id)
       .populate('tags')
-      .populate('employees')) as ICompanyDocument
+      .populate('employees')
+      .lean()) as ICompanyDocument
   } catch (err) {
     throw new Error(err as string)
   }
