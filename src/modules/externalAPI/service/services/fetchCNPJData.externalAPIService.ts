e768eccbb92e8bcef947cabe108bb10fefb445df
Igor Silvestre <igormorais.silvestre@icloud.com>
2023-11-20 18:29:01 -0300

    refactor: CNPJ interface into interface folder

--- src/modules/externalAPI/service/services/fetchCNPJData.externalAPIService.ts
+++ src/modules/externalAPI/service/services/fetchCNPJData.externalAPIService.ts
@@ -4,53 +4,8 @@ import { errorMessageKeys } from '../../../../shared/keys/errorMessageKeys'
 import { externalAPIConfigs } from '../../../../shared/externalAPIEndpoints'
 import { CACHE } from '../../../../shared/cache'
 import { CacheTime } from '../../../../shared/keys/cacheTime'
-
-interface ICNPJActivity {
-  code: string
-  text: string
-}
-
-interface ICNPJQSA {
-  nome: string
-  qual: string
-}
-
-interface ICNPJBilling {
-  free: boolean
-  database: boolean
-}
-
-interface ICNPJData {
-  abertura: string
-  situacao: string
-  tipo: string
-  nome: string
-  fantasia: string
-  porte: string
-  natureza_juridica: string
-  atividade_principal: ICNPJActivity[]
-  atividades_secundarias: ICNPJActivity[]
-  qsa: ICNPJQSA[]
-  logradouro: string
-  numero: string
-  municipio: string
-  bairro: string
-  uf: string
-  cep: string
-  telefone: string
-  data_situacao: string
-  cnpj: string
-  ultima_atualizacao: string
-  status: string
-  complemento: string
-  email: string
-  efr: string
-  motivo_situacao: string
-  situacao_especial: string
-  data_situacao_especial: string
-  capital_social: string
-  billing: ICNPJBilling
-}
+import { ICNPJData } from '../../interfaces/ICNPJData'
+import { removeSpecialCharacters } from '../../../../shared/functions/removeSpecialCharacters'
 
 function normalizeCNPJData(CNPJData: ICNPJData) {
   const normalizeCNPJData = {
@@ -75,6 +30,7 @@ function normalizeCNPJData(CNPJData: ICNPJData) {
 }
 
 export async function fetchCNPJData(cnpj: string) {
+  cnpj = removeSpecialCharacters(cnpj)
   const cachedKey = `fetchCNPJData-${cnpj}`
   const cachedData = await CACHE.get(cachedKey)
   if (cachedData) return cachedData as ICNPJData
@@ -99,4 +55,3 @@ export async function fetchCNPJData(cnpj: string) {
     })
   }
 }
-
