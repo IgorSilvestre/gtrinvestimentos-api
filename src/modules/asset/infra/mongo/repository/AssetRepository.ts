faf5a3a27d93976cdf06667682d16d6f121a018b
Igor Silvestre <igormorais.silvestre@icloud.com>
2024-04-02 09:43:06 -0300

    feat: count total assets repository

--- src/modules/asset/infra/mongo/repository/AssetRepository.ts
+++ src/modules/asset/infra/mongo/repository/AssetRepository.ts
@@ -1,7 +1,9 @@
+import { countTotalDocuments } from "./repositories/assetRepository.countTotalDocuments";
 import { create } from "./repositories/assetRespository.create";
 
 export const AssetRepository = {
   create,
+  countTotalDocuments,
   // update,
   // addToSet,
   // updateWithPull,
