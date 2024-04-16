56e38e018627e81efb2b233a7238eaa873dddc80
igor Silvestre <igormorais.silvestre@icloud.com>
2024-04-16 15:11:21 -0300

    fix: search companies tags params

--- src/modules/company/infra/express/controllers/companyController.search.ts
+++ src/modules/company/infra/express/controllers/companyController.search.ts
@@ -5,6 +5,9 @@ import { normalizeCompanies } from '../../../interfaces-validation/normalizeComp
 import { ICompaniesPaginated } from '../../../interfaces-validation/ICompaniesPaginated'
 
 export async function search(req: Request, res: Response) {
+    if(req.query.tags && typeof req.query.tags === 'string' && req.query.tags.includes(','))
+        req.query.tags = req.query.tags.split(',')
+
   const companies: ICompaniesPaginated | AppError = await CompanyService.search(
     req.query,
   )
