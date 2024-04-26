872d934c284b8a40c41dc8d256a2591f2d44b71d
igor Silvestre <igormorais.silvestre@icloud.com>
2024-04-26 14:36:11 -0300

    feat: tag domain - endpoint: find

--- src/modules/tag/infra/mongo/repository/TagRepository.ts
+++ src/modules/tag/infra/mongo/repository/TagRepository.ts
@@ -3,11 +3,13 @@ import { update } from './repositories/tagRepository.update'
 import { getById } from './repositories/tagRepository.getById'
 import { remove } from './repositories/tagRepository.remove'
 import { getAll } from './repositories/tagRepository.getAll'
+import { find } from './repositories/tagRepository.find'
 
 export const TagRepository = {
-  create,
-  update,
-  getById,
-  getAll,
-  remove,
+    create,
+    find,
+    update,
+    getById,
+    getAll,
+    remove,
 }
