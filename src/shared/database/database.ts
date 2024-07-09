6d11bce8caf9bce1983772a57e1bd33042ee4951
igor Silvestre <igormorais.silvestre@icloud.com>
2024-07-09 17:05:21 -0300

    feat: reforced handle connectio fails with mongo database

--- src/shared/database/database.ts
+++ src/shared/database/database.ts
@@ -1,31 +1,51 @@
-import mongo from 'mongoose'
-import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const url = process.env.DATABASE_MONGO_STRING || 'NO DATABASE STRING!'
-let retryCount = 1 // Counter to track the number of retries
-const maxRetries = 2 // Maximum number of retries
+const url = process.env.DATABASE_MONGO_STRING || 'NO DATABASE STRING!';
+let retryCount = 1; // Counter to track the number of retries
+const maxRetries = 5; // Maximum number of retries
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, // Timeout for initial server selection
+  socketTimeoutMS: 45000, // Timeout for socket inactivity
+  connectTimeoutMS: 10000, // Timeout for initial connection
+  // Removed unsupported options
+};
 
 const connectWithRetry = async () => {
   try {
-    await mongo.connect(url)
-    console.log('MongoDB Connected!')
+    await mongoose.connect(url, options);
   } catch (err) {
-    console.error('Failed to connect to MongoDB', err)
+    console.error('Failed to connect to MongoDB', err);
 
     if (retryCount < maxRetries) {
-      retryCount++ // Increment the retry counter
+      retryCount++; // Increment the retry counter
       console.log(
         `Retrying in 5 seconds... (Attempt ${retryCount} of ${maxRetries})`,
-      )
-      setTimeout(connectWithRetry, 5000) // Retry after 5 seconds
+      );
+      setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
     } else {
-      console.log('Max retries reached. Connection failed.')
+      console.log('Max retries reached. Connection failed.');
     }
   }
-}
+};
 
-connectWithRetry()
+connectWithRetry();
 
-export default mongo
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose: connected to the database');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose: connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose: disconnected from the database');
+});
+
+export default mongoose;
