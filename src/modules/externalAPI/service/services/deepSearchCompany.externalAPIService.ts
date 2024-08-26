66e804dea2d9240977ad91b7620933dc7952958a
igor Silvestre <igormorais.silvestre@icloud.com>
2024-08-26 17:35:38 -0300

    fix: endpoint deepSearchCompany : CNPJ search

--- src/modules/externalAPI/service/services/deepSearchCompany.externalAPIService.ts
+++ src/modules/externalAPI/service/services/deepSearchCompany.externalAPIService.ts
@@ -5,56 +5,58 @@ import { externalAPIEndpoints } from '../../../../shared/externalAPIEndpoints'
 import axios from 'axios'
 
 export async function deepSearchCompany(domain: string) {
-    let domainOwner: Record<string, any> | undefined = undefined
+  let domainOwner: Record<string, any> | undefined = undefined
 
-    const response = await axios.get(`${externalAPIEndpoints.whois}${domain}`) 
-    const output = response.data
+  const response = await axios.get(`${externalAPIEndpoints.whois}${domain}`)
+  const output = response.data
 
-    const owner = output.entities[0].legalRepresentative
+  const owner = output.entities[0].legalRepresentative
 
-    const documentType = output.entities[0].publicIds[0].type
-    const ownerid = output.entities[0].publicIds[0].identifier
+  const documentType = output.entities[0].publicIds[0].type
+  const ownerid = output.entities[0].publicIds[0].identifier
 
-    try {
-        domainOwner = output
-            ? {
-                name: owner,
-                documentType, 
-                document: ownerid,
-                // emails: emails?.toString().split(','),
-            }
-            : undefined
-    } catch (err) {
-        console.log(err)
+  try {
+    domainOwner = output
+      ? {
+        name: owner,
+        documentType,
+        document: ownerid,
+        // emails: emails?.toString().split(','),
+      }
+      : undefined
+  } catch (err) {
+    console.log(err)
+  }
+
+  try {
+    const [CNPJResponse, linkedinResponse] = await Promise.allSettled([
+      (domainOwner?.documentType === 'cnpj' && domainOwner?.document) ?
+        externalAPIService.fetchCNPJData(domainOwner.document.replace(/\D/g, ''))
+        : undefined,
+      externalAPIService.fetchLinkedinCompanyDataByDomain(domain),
+    ])
+
+
+    const CNPJData =
+      CNPJResponse.status === 'fulfilled' && CNPJResponse.value
+        ? CNPJResponse.value
+        : undefined
+    const linkedinData =
+      linkedinResponse.status === 'fulfilled'
+        ? linkedinResponse.value
+        : undefined
+
+    return {
+      CNPJData,
+      domainOwner,
+      linkedinData,
     }
-
-
-    try {
-        const [CNPJResponse, linkedinResponse] = await Promise.allSettled([
-            (domainOwner?.type === 'cnpj' && domainOwner?.document) ? externalAPIService.fetchCNPJData(domainOwner.document.replace(/\D/g, '')) : undefined,
-            externalAPIService.fetchLinkedinCompanyDataByDomain(domain),
-        ])
-
-        const CNPJData =
-            CNPJResponse.status === 'fulfilled' && CNPJResponse.value
-                ? CNPJResponse.value
-                : undefined
-        const linkedinData =
-            linkedinResponse.status === 'fulfilled'
-                ? linkedinResponse.value
-                : undefined
-
-        return {
-            CNPJData,
-            domainOwner,
-            linkedinData,
-        }
-    } catch (error) {
-        console.error(error)
-        return new AppError({
-            clientMessage: errorMessageKeys.cantSearch,
-            apiError: error,
-        })
-    }
+  } catch (error) {
+    console.error(error)
+    return new AppError({
+      clientMessage: errorMessageKeys.cantSearch,
+      apiError: error,
+    })
+  }
 }
 
