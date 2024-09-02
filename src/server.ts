bafa99205f02b3024c8f667d97ccfc9644ccef57
igor Silvestre <igormorais.silvestre@icloud.com>
2024-09-02 16:20:24 -0300

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
 import express from 'express'
-import cors from 'cors'
 import { router } from './http/router'
 import { connectToS3 } from './shared/database/s3/s3Connection'
 
 const app = express()
 
-app.use(cors({
-    origin: '*',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization'
-}))
+const allowedDomains = [
+  'https://gtrinvestimentos-frontend.vercel.app',
+  'http://localhost:5173'
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  // @ts-ignore
+  if (allowedDomains.indexOf(origin) !== -1) {
+    res.header('Access-Control-Allow-Origin', origin); // Set the origin if it's allowed
+  } else {
+    res.header('Access-Control-Allow-Origin', '*'); // Alternatively, allow all (use with caution)
+  }
+  
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(express.json())
 
@@ -19,7 +31,7 @@ app.use('/api', router)
 
 const PORT = process.env.PORT || 3005
 app.listen(PORT, () => {
-    console.log(`GTRINVESTIMENTOS-API running on PORT=${PORT}`),
+  console.log(`GTRINVESTIMENTOS-API running on PORT=${PORT}`),
     connectToS3()
 })
 
